05d4943aeb0ced14036dd43df13a56cb82589696
adamcesco <acescobedo@smu.edu>
2024-01-26 20:48:48 -0600

    changes solution class to namespace

--- Solution.h
+++ Solution.h
@@ -2,29 +2,19 @@
 #define THINKCELLASSIGNMENT_SOLUTION_H
 #include <bits/stdc++.h>
 
-class SOLUTION {
-private:
-
-public:
-    SOLUTION() = default;
-    ~SOLUTION() = default;
-    
-    int bruteForceSolution(int val);
-    int optimizedSolution(int val);
-};
-
+namespace SOLUTION {
 
 // ------- SECTION FOR BRUTE FORCE SOLUTION -------
-int SOLUTION::bruteForceSolution(int val){
+int bruteForceSolution(int val){
     return 0;
 }
 
 
 // ------- SECTION FOR OPTIMIZED SOLUTION -------
-int SOLUTION::optimizedSolution(int val){
+int optimizedSolution(int val){
     return 0;
 }
 
-
+}; // namespace SOLUTION
 
 #endif
